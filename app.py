b32d11a320fd9b97cafa5f3f9e3742b07b30094f
salvgrd <salvgrd@gmail.com>
2021-01-31 23:37:05 +0100

    [fix] uses DictCursor instead

--- app.py
+++ app.py
@@ -21,21 +21,8 @@ def get_products():
     # Add pagination
     c = mysql.connection.cursor()
     c.execute('SELECT * FROM products')
-    keys =  [i[0] for i in c.description]
-    products = []
-    for p in c.fetchall():
-        product = {
-            keys[0]: p[0],
-            keys[1]: p[1],
-            keys[2]: p[2],
-            keys[3]: p[3],
-            keys[4]: p[4],
-            keys[5]: p[5],
-            keys[6]: p[6],
-            keys[7]: p[7]
-        }
-        products.append(product)
-    return jsonify(products)
+    products = c.fetchall()
+    return jsonify({"data": products})
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
