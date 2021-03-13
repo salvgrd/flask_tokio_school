d3275ed93b4913b44df487ea23295dcbd69f2df3
salvgrd <salvgrd@gmail.com>
2021-03-13 01:51:36 +0100

    [feat] folder structure

--- trash/core/__init__.py
+++ trash/core/__init__.py
@@ -10,7 +10,7 @@ with open(f"{basedir}\core\dbconfig.json") as f:
   dbconfig = json.load(f)
 
 for key, value in dbconfig.items():
-    dbURI = dbURI.replace(key, value)
+  dbURI = dbURI.replace(key, value)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
